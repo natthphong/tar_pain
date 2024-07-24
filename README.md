9e83720fa3d03a9ad1ce59a68036da89c9c95a51
natthaphong <tazamaak07@gmail.com>
2024-07-24 16:25:12 +0700

    add README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tar_pain
+
+# setup
+    brew install tar_paint
+    pip install tar_paint
+# start open terminal
+    tar_paint
--- tar_paint.rb
+++ tar_paint.rb
@@ -3,13 +3,15 @@ class TarPaint < Formula
 
   desc "natthphong application"
   homepage "https://github.com/natthphong/tar_paint"
-  url "https://github.com/natthphong/tar_paint/archive/v0.1.0.tar.gz"
-  sha256 "SHA256_CHECKSUM_OF_YOUR_TAR_GZ"
+  url "https://files.pythonhosted.org/packages/d5/61/3d77e5c85ae270779d333c94178a7fe549def45b03ea18b08eed6868302a/tar_paint-0.1.0.tar.gz"
+  sha256 "5571b88d5e27f388ef88cf898fc346726b141fadab137bd28ee5c97cd4e753d0"
 
-  depends_on "python@3.9"
+  depends_on "python@3.8"
+
 
   def install
     virtualenv_install_with_resources
+    bin.install_symlink libexec/"bin/tar_paint"
   end
 
   test do
